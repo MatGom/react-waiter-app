808508d5aa029dfe8a01461e89db3680b8e6bd4f
Mateusz Gomolka <mdm.gomolka@gmail.com>
2023-04-21 21:44:58 +0100

    Add Footer

--- src/components/views/Footer/Footer.js
+++ src/components/views/Footer/Footer.js
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <p className='text-center text-muted m-auto py-3'>Copyright © WaiterApp 2023</p>
+    </footer>
+  );
 };
 
 export default Footer;
