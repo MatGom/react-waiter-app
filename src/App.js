ae773a4e1023bf3e52233083de2ca707b0b34ce7
Mateusz Gomolka <mdm.gomolka@gmail.com>
2023-04-21 21:42:49 +0100

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/views/Header/Header';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home/Home';
 import TablePage from './components/pages/TablePage/TablePage';
 import NotFound from './components/pages/NotFound/NotFound';
+import Footer from './components/views/Footer/Footer';
 
 const App = () => {
   return (
-    <div>
+    <Container>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/table/:id' element={<TablePage />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
-    </div>
+      <Footer />
+    </Container>
   );
 };
 
---
+++ src/components/views/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <Navbar bg='primary' variant='dark' expand='lg' className='mt-4 mb-4 rounded'>
+      <Container>
+        <Navbar.Brand as={NavLink} to='/'>
+          Waiter.app
+        </Navbar.Brand>
+        <Nav className='ms-auto'>
+          <Nav.Link as={NavLink} to='/'>
+            Home
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
